0369a409c8131f4a2fd02ee4250f9bd5b51d3235
i-kogda-nibud-nas-ne-budet <dansparrow32@gmail.com>
2024-01-25 09:43:16 +0300

    add pump and etc

---
+++ cad_signal.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def get_signals(filename):
+    df = pd.read_excel(filename + '_obj.xls', engine='xlrd')
+    df = df[['INDEX', 'Имя']]
+    mask = (
+        df['INDEX'].str.len() > 0) & (
+        ~df["INDEX"].str.contains('_', na=False))
+    df.loc[mask, 'INDEX'] = df['INDEX'].astype(str) + '_XQ02'
+    df = df[df["INDEX"].str.contains('_', na=False)]
+    df[['KKS', 'signal']] = df['INDEX'].str.split('_', expand=True)
+    df.to_excel(filename + '_signal.xls', index=False)
+
+
+get_signals('30CWA10EP571')
